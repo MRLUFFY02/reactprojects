1fd09789264db4d00842f55660661c71e041db13
MRLUFFY02 <arvind080318@gmail.com>
2024-03-13 12:27:02 +0530

    created component

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+//component--js function returns only one JSX element
+//component first letter must be capital
+function App()
+ {
+
+  //JSX starts
+  return (
+    <div className="App">
+      <h1>Welcome ReactJs⭐👌👌!!!</h1>
+    </div>
+  );
+  //JSX ends
+}
+
+export default App;
---
+++ header.js
@@ -0,0 +1,9 @@
+function Header()
+{
+   
+    return (<div className="header"> <h1>Header</h1>
+            <h2>Welcome ReactJs⭐👌👌!!1</h2>
+         </div>)
+}
+
+export default Header
