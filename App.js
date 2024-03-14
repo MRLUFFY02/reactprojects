f547cf48190e9941793aa88ca1e346928ef40466
MRLUFFY02 <arvind080318@gmail.com>
2024-03-14 12:21:19 +0530

    array of user in  Msg component

--- App.js
+++ App.js
@@ -4,28 +4,31 @@ import './App.css';
 //component first letter must be capital
 function App()
  {
+  const users=[{name:"Aravind", pic:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfQ1r9t0OtolRp6JzSQ5UJhn0w86DHmeugOeRXCUp0ZIT0QuIB"},{name:"Nirmal", pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4kVoohSv99m7iH8irz5yKQe2YrA6zXRfLxDvPyoGuNYGlU4Xw"},{name:"Balaji" ,pic:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYWgIy_ffTNaNuIvCsNT9aYcXzR_wEwM3WzMue6y9SdPfHziTk"},{name:"Mohan", pic:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfQ1r9t0OtolRp6JzSQ5UJhn0w86DHmeugOeRXCUp0ZIT0QuIB"},{name:"Siva", pic:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfQ1r9t0OtolRp6JzSQ5UJhn0w86DHmeugOeRXCUp0ZIT0QuIB"},{name:"Vijay", pic:"https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfQ1r9t0OtolRp6JzSQ5UJhn0w86DHmeugOeRXCUp0ZIT0QuIB"}]
 
   //JSX starts
   return (
     <div className="App">
       <h1>Welcome ReactJs⭐👌👌!!!</h1>
       <div className='container'>
-      <Msg name="Aravind" pic="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSfQ1r9t0OtolRp6JzSQ5UJhn0w86DHmeugOeRXCUp0ZIT0QuIB"/>
-      <Msg name="Nirmal" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4kVoohSv99m7iH8irz5yKQe2YrA6zXRfLxDvPyoGuNYGlU4Xw"/>
-      <Msg name="Balaji" pic="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYWgIy_ffTNaNuIvCsNT9aYcXzR_wEwM3WzMue6y9SdPfHziTk"/>
+        <Msg users={users}/>
       </div>
 
     </div>
   );
   //JSX ends
 }
-function Msg(props)
+function Msg({users})
 {
   return(
-    <div className='msg'>
-      <h1>Hello {props.name}</h1>
-      <img src={props.pic}/>
-    </div>
+      
+        users.map(user=>
+          <div id={user.name} className='usercard'>
+      <h1>Hello {user.name}</h1>
+      <img src={user.pic}/>
+    </div>) 
+      
+    
   )
 }
 
